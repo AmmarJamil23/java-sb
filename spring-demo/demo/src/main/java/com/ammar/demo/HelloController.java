189846077b1e30df1e60cafd85d76956c1cbe223
AmmarJamil23 <ammarjamil7000@gmail.com>
2026-02-22 16:23:04 +0500

    spring project initialzied and customer model created

---
+++ spring-demo/demo/src/main/java/com/ammar/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.ammar.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello customer, spring bot is workin";
+    }
+
+    @GetMapping("/health")
+    public String hello2() {
+        return "Hello customer, this is / health route";
+    }
+    
+}
---
+++ spring-demo/demo/src/main/java/com/ammar/demo/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.ammar.demo.repository;
+
+import com.ammar.demo.model.Customer;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;;
+
+public class CustomerRepository {
+    
+}
